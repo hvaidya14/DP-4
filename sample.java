4f4dd30609604c8387c57f3616a6380bdad6f126
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-02-26 05:34:18 -0800

    Update sample.java

--- sample.java
+++ sample.java
@@ -16,3 +16,24 @@ class Solution {
         return max*max;
     }
 }
+class Solution {
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+        int n = arr.length;
+        int dp[] = new int[n];
+        int max =0;
+        dp[0] = arr[0];
+        for (int i=1;i<arr.length;i++){
+            max=arr[i];
+            for (int j=1;j<=k && i-j+1 >=0;j++) {
+                max = Math.max(max, arr[i-j+1]);
+                if (i-j >=0) {
+                    dp[i] = Math.max(dp[i], dp[i-j] + j*max);
+                } else {
+                    dp[i] = Math.max(dp[i], j*max);
+                }
+
+            }
+        }
+        return dp[n-1];
+    }
+}
